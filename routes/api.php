bba6e2ecc07340269e895906756d16db428ef49d
zinocchi <zinocchidev@gmail.com>
2026-01-06 19:27:57 +0700

    api register

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', fn(Request $request) => [
         'user' => $request->user()
